0d9717ad6a3a4c182d9af8987823cf94dfe70196
sulaimon.buari <sulaimon.buari@PROC04-APPDEV07>
2019-12-10 18:49:42 +0100

    dashboard at 80%

--- src/app/portal/dashboard/dashboard.module.ts
+++ src/app/portal/dashboard/dashboard.module.ts
@@ -3,13 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard.routing';
 import { DashboardComponent } from './container/dashboard.component';
-
+import { NgApexchartsModule } from 'ng-apexcharts';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatRippleModule,
+  MatFormFieldModule,
+  MatTooltipModule,
+  MatSelectModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NgApexchartsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule
   ]
 })
 export class DashboardModule { }
